447de76ee50cabd954da28a83bc209914d42b16d
M Taylor Long <longx598@umn.edu>
2014-05-14 15:31:44 -0500

    Committing TaylorTweaks to Master

--- SpecialOverlay.py
+++ SpecialOverlay.py
@@ -7,7 +7,7 @@
 # features) to append to the target layer (overlay features).
 # ---------------------------------------------------------------------------
 
-import arcpy, sys
+import arcpy
 
 # Read in parameters
 #  1. Data features
@@ -19,13 +19,14 @@ import arcpy, sys
 data = arcpy.GetParameterAsText(0)
 overlay = arcpy.GetParameterAsText(1)
 field = arcpy.GetParameterAsText(2)
-expression = arcpy.GetParameterAsText(3)        # default value is hard coded as SUM
+expression = arcpy.GetParameterAsText(3)	# default value is hard coded as SUM
 output = arcpy.GetParameterAsText(4)
 
 # Create field name variables
-areaOrig = "AREA"                               # original area
+areaOrig = "AREA"	# original area
 ratio = "RATIO"
-newVal = "VAL_" + str(expression)
+newVal = str(field) + "_" + "NEW"
+finalVal = str(field) + "_" + str(expression)
 
 # Add new field to data features
 arcpy.AddField_management(data, areaOrig, "FLOAT")
@@ -36,7 +37,7 @@ arcpy.CalculateField_management(data, areaOrig, "!shape.area@squaremeters!", "PY
 arcpy.AddMessage(areaOrig + " field calculated")
 
 # Identity: computes geometric intersection between two feature classes (intermediate data)
-dataSlice = arcpy.Identity_analysis(data, overlay, "in_memory", "NO_FID")
+dataSlice = arcpy.Identity_analysis(data, overlay, "overlay_temp", "NO_FID")
 arcpy.AddMessage("Identity tool completed")
 
 # Add ratio field to dataSlice (identity) feature class
@@ -53,11 +54,12 @@ arcpy.AddField_management(dataSlice, newVal , "FLOAT")
 # Create variable for ratio formula
 formula = "!" + str(field) + "! * !RATIO!"
 
-# Calculate new value field based on ratio fieldarcpy.CalculateField_management(dataSlice, newVal, formula, "PYTHON_9.3", "")
+# Calculate new value field based on ratio field
+arcpy.CalculateField_management(dataSlice, newVal, formula, "PYTHON_9.3", "")
 arcpy.AddMessage("New " + field + " field calculated")
 
-# Spatial join slice and overlay using correct summary method
-#-------------------------------------------------------------------------
+# Field mapping to prepare for Spatial Join
+#------------------------------------------
 
 # Create a new fieldmappings and add the two input feature classes
 fieldmappings = arcpy.FieldMappings()
@@ -71,24 +73,28 @@ fieldmap = fieldmappings.getFieldMap(fieldIndex)
 finalField = fieldmap.outputField
 
 # Rename the field and pass the updated field object back into the field map
-finalField.name = "VAL_FINAL"
-finalField.aliasName = "Final Value"
+finalField.name = finalVal
+finalField.aliasName = finalVal
 fieldmap.outputField = finalField
 
 # Set the merge rule
-#if expression == "MEAN":
-#   fieldmap.mergeRule = 'Mean'
-#else:
-fieldmap.mergeRule = "Sum"
+if expression == "MEAN":
+    fieldmap.mergeRule = "Mean"
+elif expression == "MEDIAN":
+    fieldmap.mergeRule = "Median"
+else:
+    fieldmap.mergeRule = "Sum"
 
 # Replace old field map with the updated one 
 fieldmappings.replaceFieldMap(fieldIndex, fieldmap)
+
+#------------------------------------------
 
 #Run the Spatial Join tool
 arcpy.SpatialJoin_analysis(overlay, dataSlice, output, "JOIN_ONE_TO_ONE", "KEEP_ALL", fieldmappings, "CONTAINS")
 arcpy.AddMessage("Spatial Join completed")
 arcpy.AddMessage(output + "created")
 
-# Delete area field from data features
-# arcpy.DeleteField_management(data, area)
+# Delete in memory file
+arcpy.Delete_management(dataSlice)
 arcpy.AddMessage("\n")
